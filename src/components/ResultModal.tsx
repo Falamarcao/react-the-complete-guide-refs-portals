3bdb97e57052b2ce76f8f981a2e4278c51da048a
falamarcao <falamarcao@gmail.com>
2024-10-28 23:58:13 -0300

    Exposing Component APIs via the useImperativeHandle Hook

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -1,28 +1,37 @@
-import { forwardRef } from 'react';
+import { forwardRef, useRef, useImperativeHandle } from 'react';
 
 interface ResultModalProps {
   result: string;
   targetTime: number;
 }
 
-const ResultModal = forwardRef<HTMLDialogElement, ResultModalProps>(
-  ({ result, targetTime }, ref) => {
-    return (
-      <dialog ref={ref} className="result-modal">
-        <h2>You {result}</h2>
-        <p>
-          The target time was <strong>{targetTime} seconds.</strong>
-        </p>
-        <p>
-          You stopped the timer with <strong>X secounds left.</strong>
-        </p>
-        <form method="dialog">
-          <button>Close</button>{' '}
-          {/* A button inside a form method=dialog automatically closes the dialog without any JS. */}
-        </form>
-      </dialog>
-    );
-  }
-);
+const ResultModal = forwardRef<
+  Pick<HTMLDialogElement, 'showModal'>,
+  ResultModalProps
+>(({ result, targetTime }, ref) => {
+  const dialog = useRef<HTMLDialogElement>(null);
+
+  useImperativeHandle(ref, () => ({
+    showModal: () => {
+      dialog.current?.showModal();
+    },
+  }));
+
+  return (
+    <dialog ref={dialog} className="result-modal">
+      <h2>You {result}</h2>
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with <strong>X secounds left.</strong>
+      </p>
+      <form method="dialog">
+        <button>Close</button>{' '}
+        {/* A button inside a form method=dialog automatically closes the dialog without any JS. */}
+      </form>
+    </dialog>
+  );
+});
 
 export default ResultModal;
