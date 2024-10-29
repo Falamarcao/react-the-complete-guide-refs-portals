62f60e7840b93c7960a01501f811b97895e08abd
falamarcao <falamarcao@gmail.com>
2024-10-28 21:49:53 -0300

    Repetition: Managing User Input with State (Two-Way-Binding)

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,25 @@
+import { ChangeEvent, useState } from 'react';
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState<string | null>(null);
+  const [isSet, setIsSet] = useState<boolean>(false);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (isSet) setIsSet(false);
+
+    setPlayerName(event.target.value);
+  };
+
+  const handleClick = (): void => {
+    setIsSet(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isSet ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
