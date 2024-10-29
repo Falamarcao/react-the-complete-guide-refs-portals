880d619e1d3059dbc1b1e2bcd8572641bbe3c32f
falamarcao <falamarcao@gmail.com>
2024-10-28 23:23:58 -0300

    Forwarding Refs to Custom Components

--- src/components/ResultModal.tsx
+++ src/components/ResultModal.tsx
@@ -1,24 +1,28 @@
+import { forwardRef } from 'react';
+
 interface ResultModalProps {
   result: string;
   targetTime: number;
 }
 
-const ResultModal = ({ result, targetTime }: ResultModalProps) => {
-  return (
-    <dialog className="result-modal" open>
-      <h2>You {result}</h2>
-      <p>
-        The target time was <strong>{targetTime} seconds.</strong>
-      </p>
-      <p>
-        You stopped the timer with <strong>X secounds left.</strong>
-      </p>
-      <form method="dialog">
-        <button>Close</button>{' '}
-        {/* A button inside a form method=dialog automatically closes the dialog without any JS. */}
-      </form>
-    </dialog>
-  );
-};
+const ResultModal = forwardRef<HTMLDialogElement, ResultModalProps>(
+  ({ result, targetTime }, ref) => {
+    return (
+      <dialog ref={ref} className="result-modal">
+        <h2>You {result}</h2>
+        <p>
+          The target time was <strong>{targetTime} seconds.</strong>
+        </p>
+        <p>
+          You stopped the timer with <strong>X secounds left.</strong>
+        </p>
+        <form method="dialog">
+          <button>Close</button>{' '}
+          {/* A button inside a form method=dialog automatically closes the dialog without any JS. */}
+        </form>
+      </dialog>
+    );
+  }
+);
 
 export default ResultModal;
--- src/components/TimerChallenge.tsx
+++ src/components/TimerChallenge.tsx
@@ -8,44 +8,50 @@ interface TimerChallengeProps {
 }
 
 const TimerChallenge = ({ title, targetTime }: TimerChallengeProps) => {
-  const [isTimerStarted, setIsTimerStarted] = useState<boolean>(false);
-  const [isTimerExpired, setIsTimerExpired] = useState<boolean>(false);
+    const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const timerRef = useRef<NodeJS.Timeout>();
+    const [isTimerStarted, setIsTimerStarted] = useState<boolean>(false);
+    const [isTimerExpired, setIsTimerExpired] = useState<boolean>(false);
 
-  const handleStart = () => {
-    timerRef.current = setTimeout(() => {
-      setIsTimerExpired(true);
-    }, targetTime * 1000);
+    const timerRef = useRef<NodeJS.Timeout>();
 
-    setIsTimerStarted(true);
-  };
+    const handleStart = () => {
+      timerRef.current = setTimeout(() => {
+        setIsTimerExpired(true);
+        dialogRef.current?.showModal();
+      }, targetTime * 1000);
 
-  const handleStop = () => {
-    clearTimeout(timerRef.current);
-  };
+      setIsTimerStarted(true);
+    };
 
-  return (
-    <>
-      {isTimerExpired && <ResultModal result="lost" targetTime={targetTime} />}
+    const handleStop = () => {
+      clearTimeout(timerRef.current);
+    };
 
-      <section className="challenge">
-        <h2>{title}</h2>
+    return (
+      <>
+        <ResultModal
+          ref={dialogRef}
+          result={isTimerExpired ? 'lost' : 'win'}
+          targetTime={targetTime}
+        />
+        <section className="challenge">
+          <h2>{title}</h2>
 
-        <p className="challenge-time">
-          {targetTime} second{targetTime > 0 ? 's' : ''}
-        </p>
-        <p>
-          <button onClick={isTimerStarted ? handleStop : handleStart}>
-            {isTimerStarted ? 'Stop' : 'Start'} Challenge
-          </button>
-        </p>
-        <p className={isTimerStarted ? 'active' : undefined}>
-          {isTimerStarted ? 'Time is running...' : 'Timer inactive'}
-        </p>
-      </section>
-    </>
-  );
+          <p className="challenge-time">
+            {targetTime} second{targetTime > 0 ? 's' : ''}
+          </p>
+          <p>
+            <button onClick={isTimerStarted ? handleStop : handleStart}>
+              {isTimerStarted ? 'Stop' : 'Start'} Challenge
+            </button>
+          </p>
+          <p className={isTimerStarted ? 'active' : undefined}>
+            {isTimerStarted ? 'Time is running...' : 'Timer inactive'}
+          </p>
+        </section>
+      </>
+    );
 };
 
 export default TimerChallenge;
